2fdd1e0e9c8202c748a839bac18e4aa01e11369c
FINNSEEFLY <matmak122@gmail.com>
2020-11-02 03:47:12 +0300

    Initial commit

---
+++ WaitAll/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using ConsoleCopyPaste;
+
+namespace WaitAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskDelegate[] delegates =
+            {
+                () => Console.WriteLine(1), () => Console.WriteLine(2), () => Console.WriteLine(3),
+                () => Console.WriteLine(4),() => Console.WriteLine(5),() => Console.WriteLine(6),() => Console.WriteLine(7)
+            };
+            Parallel.WaitAll(delegates);
+            Console.WriteLine("This line must be the last one");
+        }
+
+        static class Parallel
+        {
+            public static void WaitAll(TaskDelegate[] delegates)
+            {
+                var taskQueue = new TaskQueue();
+                var delegatesCount = delegates.Length;
+                var counter = 0;
+                for (var i = 0; i < delegates.Length; i++)
+                {
+                    delegates[i] = delegates[i] + (() => counter++);
+                    taskQueue.EnqueueTask(delegates[i]);
+                }
+
+                while (counter != delegatesCount)
+                {
+                    Thread.Yield();
+                }
+                taskQueue.Dispose();
+            }
+        }
+    }
+}
---
+++ WaitAll/TaskQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ConsoleCopyPaste
+{
+    public delegate void TaskDelegate();
+    
+    public class TaskQueue : IDisposable
+    {
+        private readonly ConcurrentQueue<TaskDelegate> _taskPull;
+        private bool disposed = false;
+        private const int DefaultNumOfThreads = 8;
+        
+        public TaskQueue(int numOfThreads)
+        {
+            _taskPull = new ConcurrentQueue<TaskDelegate>();
+            for (var i = 0; i < numOfThreads; i++)
+            {
+                new Thread(ExecuteTasks).Start();
+            }
+        }
+
+        public TaskQueue()
+        {
+            _taskPull = new ConcurrentQueue<TaskDelegate>();
+            for (var i = 0; i < DefaultNumOfThreads; i++)
+            {
+                new Thread(ExecuteTasks).Start();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            if (disposed)
+                throw new ObjectDisposedException(null);
+            _taskPull.Enqueue(task);
+        }
+
+        public void Dispose()
+        {
+            if (disposed) return;
+                disposed = true;
+        }
+
+        private void ExecuteTasks()
+        {
+            while (!disposed)
+            {
+                _taskPull.TryDequeue(out var task);
+                if (task == null)
+                {
+                    Thread.Sleep(30);
+                }
+                else
+                {
+                    task();
+                }
+            }
+        }
+    }
+}
