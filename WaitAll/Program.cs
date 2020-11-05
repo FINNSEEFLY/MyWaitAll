2fd73024a66fce1b38778b0ec345dfc2fc6af0f7
FINNSEEFLY <matmak122@gmail.com>
2020-11-05 16:35:55 +0300

    Initialization refactoring

--- WaitAll/Program.cs
+++ WaitAll/Program.cs
@@ -8,17 +8,12 @@ namespace WaitAll
     {
         static void Main(string[] args)
         {
-            TaskDelegate[] delegates =
+            var delegates = new TaskDelegate[30];
+            for (var i = 0; i < 30; i++)
             {
-                () => Console.WriteLine(1), () => Console.WriteLine(2), () => Console.WriteLine(3),
-                () => Console.WriteLine(4),() => Console.WriteLine(5),() => Console.WriteLine(6),() => Console.WriteLine(7),
-                () => Console.WriteLine(8),() => Console.WriteLine(9),() => Console.WriteLine(10),() => Console.WriteLine(11),
-                () => Console.WriteLine(12),() => Console.WriteLine(13),() => Console.WriteLine(14),() => Console.WriteLine(15),
-                () => Console.WriteLine(16),() => Console.WriteLine(17),() => Console.WriteLine(18),() => Console.WriteLine(19),
-                () => Console.WriteLine(20),() => Console.WriteLine(21),() => Console.WriteLine(22),() => Console.WriteLine(23),
-                () => Console.WriteLine(24),() => Console.WriteLine(25),() => Console.WriteLine(26),() => Console.WriteLine(27),
-                () => Console.WriteLine(28),() => Console.WriteLine(29),() => Console.WriteLine(30)
-            };
+                var i1 = i;
+                delegates[i] = () => Console.WriteLine(i1 + 1);
+            }
             Parallel.WaitAll(delegates);
             Console.WriteLine("This line must be the last one");
         }
